f568390bbb1de897a3c59c80d2c4f3aadef556fb
Pakin Kraisin <dxpakin@gmail.com>
2023-05-31 15:21:30 +0700

    fix: spec file move to appropriate location and update tests to be more clear to read

--- spec/p2_spec.rb
+++ spec/p2_spec.rb
@@ -5,17 +5,38 @@ require './p2'
 RSpec.describe(Solve2) do
   it 'solves problem 2 kekw' do
     s = Solve2.new(nil)
+    # nil tree
     expect(s.solve).to eq(0)
+
     s = Solve2.new(Tree.new(1, nil, nil))
+    # 1
     expect(s.solve).to eq(1)
+
     t = Tree.new(8, Tree.new(2, Tree.new(8, nil, nil), Tree.new(7, nil, nil)), Tree.new(6, nil, nil))
     s = Solve2.new(t)
+    #     8
+    #    / \
+    #   2   6
+    #  / \
+    # 8   7
     expect(s.solve).to eq(2)
+
     t = Tree.new(8, Tree.new(2, Tree.new(8, nil, nil), Tree.new(7, nil, nil)), Tree.new(10, nil, nil))
     s = Solve2.new(t)
+    #     8
+    #    / \
+    #   2   10
+    #  / \
+    # 8   7
     expect(s.solve).to eq(3)
+
     t = Tree.new(10, Tree.new(2, Tree.new(8, nil, nil), Tree.new(7, nil, nil)), Tree.new(6, nil, nil))
     s = Solve2.new(t)
+    #    10
+    #    / \
+    #   2   6
+    #  / \
+    # 8   7
     expect(s.solve).to eq(1)
   end
 end
