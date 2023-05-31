e73425ddde995e3524f584d0a53d60d20f7389ea
Pakin Kraisin <dxpakin@gmail.com>
2023-05-31 19:08:54 +0700

    refactor: 1st refac according to comments

--- p3.rb
+++ p3.rb
@@ -2,69 +2,74 @@
 
 # solver class for p3
 class Solve3
-  def initialize
-    @a_cnt = 0
-    @b_cnt = 0
-    @c_cnt = 0
-    @res = String.new
-  end
-
   def add_char(char_to_add)
     add_cnt = @res.end_with?(char_to_add) ? 1 : 2
     case char_to_add
-    when 'a'
-      add_cnt = @a_cnt if add_cnt > @a_cnt
-      @res.concat(char_to_add * add_cnt)
-      @a_cnt -= add_cnt
-      if @b_cnt >= @c_cnt
-        @res.concat('b')
-        @b_cnt -= 1
-      else
-        @res.concat('c')
-        @c_cnt -= 1
-      end
-    when 'b'
-      add_cnt = @b_cnt if add_cnt > @b_cnt
-      @res.concat(char_to_add * add_cnt)
-      @b_cnt -= add_cnt
-      if @a_cnt >= @c_cnt
-        @res.concat('a')
-        @a_cnt -= 1
-      else
-        @res.concat('c')
-        @c_cnt -= 1
-      end
-    when 'c'
-      add_cnt = @c_cnt if add_cnt > @c_cnt
-      @res.concat(char_to_add * add_cnt)
-      @c_cnt -= add_cnt
-      if @a_cnt >= @b_cnt
-        @res.concat('a')
-        @a_cnt -= 1
-      else
-        @res.concat('b')
-        @b_cnt -= 1
-      end
+    when 'a' then add_a(add_cnt)
+    when 'b' then add_b(add_cnt)
+    when 'c' then add_c(add_cnt)
     end
   end
 
-  def addEnding(char_to_add)
-    count = 0
-    case char_to_add
-    when 'a'
-      count = @a_cnt
-    when 'b'
-      count = @b_cnt
-    when 'c'
-      count = @c_cnt
+  def add_a(add_cnt)
+    add_cnt = @a_cnt if add_cnt > @a_cnt
+    @res.concat('a' * add_cnt)
+    @a_cnt -= add_cnt
+    if @b_cnt >= @c_cnt
+      @res.concat('b')
+      @b_cnt -= 1
+    else
+      @res.concat('c')
+      @c_cnt -= 1
     end
-    return unless count > 0
+  end
 
-    if @res.size == 0 or !@res.end_with?(char_to_add)
-      @res.concat(char_to_add * (count >= 2 ? 2 : 1))
-    elsif @res.size == 1 or !@res.end_with?(char_to_add * 2)
-      @res.concat(char_to_add)
+  def add_b(add_cnt)
+    add_cnt = @b_cnt if add_cnt > @b_cnt
+    @res.concat('b' * add_cnt)
+    @b_cnt -= add_cnt
+    if @a_cnt >= @c_cnt
+      @res.concat('a')
+      @a_cnt -= 1
+    else
+      @res.concat('c')
+      @c_cnt -= 1
     end
+  end
+
+  def add_c(add_cnt)
+    add_cnt = @c_cnt if add_cnt > @c_cnt
+    @res.concat('c' * add_cnt)
+    @c_cnt -= add_cnt
+    if @a_cnt >= @b_cnt
+      @res.concat('a')
+      @a_cnt -= 1
+    else
+      @res.concat('b')
+      @b_cnt -= 1
+    end
+  end
+
+  def add_ending(char_to_add)
+    count =
+      case char_to_add
+      when 'a' then @a_cnt
+      when 'b' then @b_cnt
+      when 'c' then @c_cnt
+      end
+    return if count.zero?
+
+    if can_add_two(char_to_add) then @res.concat(char_to_add * (count >= 2 ? 2 : 1))
+    elsif can_add(char_to_add) then  @res.concat(char_to_add)
+    end
+  end
+
+  def can_add(char_to_add)
+    @res.size == 1 || !@res.end_with?(char_to_add * 2)
+  end
+
+  def can_add_two(char_to_add)
+    @res.empty? || !@res.end_with?(char_to_add)
   end
 
   def solve(a_cnt, b_cnt, c_cnt)
@@ -73,8 +78,8 @@ class Solve3
     @b_cnt = b_cnt
     @c_cnt = c_cnt
 
-    while (@a_cnt > 0 ? 1 : 0) + (@b_cnt > 0 ? 1 : 0) + (@c_cnt > 0 ? 1 : 0) > 1
-      if @a_cnt >= @b_cnt and @a_cnt >= @c_cnt
+    while (@a_cnt.nonzero? ? 1 : 0) + (@b_cnt.nonzero? ? 1 : 0) + (@c_cnt.nonzero? ? 1 : 0) > 1
+      if @a_cnt >= @b_cnt && @a_cnt >= @c_cnt
         add_char('a')
       elsif @b_cnt >= @a_cnt && @b_cnt >= @c_cnt
         add_char('b')
@@ -83,9 +88,9 @@ class Solve3
       end
     end
 
-    addEnding('a')
-    addEnding('b')
-    addEnding('c')
+    add_ending('a')
+    add_ending('b')
+    add_ending('c')
 
     @res
   end
--- spec/p3_spec.rb
+++ spec/p3_spec.rb
@@ -2,20 +2,37 @@
 
 require './p3'
 RSpec.describe(Solve3) do
-  it 'solves p3 hehe' do
-    s = Solve3.new
-    expect(s.solve(1, 1, 1)).to eq('abc')
-    expect(s.solve(1, 1, 0)).to eq('ab')
-    expect(s.solve(1, 0, 0)).to eq('a')
-    expect(s.solve(0, 0, 0)).to eq('')
+  before do
+    @s = Solve3.new
+  end
 
-    expect(s.solve(3, 2, 3)).not_to include('aaa', 'bbb', 'ccc')
-    expect(s.solve(3, 2, 3).size).to eq(8)
+  describe 'easy cases' do
+    it 'solves correctly' do
+      expect(s.solve(1, 1, 1)).to eq('abc')
+      expect(s.solve(1, 1, 0)).to eq('ab')
+      expect(s.solve(1, 0, 0)).to eq('a')
+      expect(s.solve(0, 0, 0)).to eq('')
+    end
+  end
 
-    expect(s.solve(7, 1, 2)).not_to include('aaa', 'bbb', 'ccc')
-    expect(s.solve(7, 1, 2).size).to eq(10)
+  describe 'bigger case' do
+    it 'solves correctly' do
+      expect(s.solve(3, 2, 3)).not_to include('aaa', 'bbb', 'ccc')
+      expect(s.solve(3, 2, 3).size).to eq(8)
+    end
+  end
 
-    expect(s.solve(5, 6, 1)).not_to include('aaa', 'bbb', 'ccc')
-    expect(s.solve(5, 6, 1).size).to eq(12)
+  describe 'one large number case' do
+    it 'solves correctly' do
+      expect(s.solve(7, 1, 2)).not_to include('aaa', 'bbb', 'ccc')
+      expect(s.solve(7, 1, 2).size).to eq(10)
+    end
+  end
+
+  describe 'two large number case' do
+    it 'solves correctly' do
+      expect(s.solve(5, 6, 1)).not_to include('aaa', 'bbb', 'ccc')
+      expect(s.solve(5, 6, 1).size).to eq(12)
+    end
   end
 end
